6ec84759dd4c28342ad41ff763568d1a2aa05bed
Zvonimir Pavlović <zvonimir.pavlovic@gauss.hr>
2022-04-27 18:55:47 +0200

    initial commit

---
+++ demoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demoApp
+//
+//  Created by Zvonimir Pavlović on 27.04.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
